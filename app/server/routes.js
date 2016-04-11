dca18d7cca3f0cdea1e556189c5d94b9c33452ed
Bhaskar Goyal <bhaskargoyal24@gmail.com>
2016-04-11 11:28:24 +0530

    removed test console commands

--- app/server/routes.js
+++ app/server/routes.js
@@ -16,7 +16,6 @@ module.exports = function(app) {
 		var dbconnect = require('./db/connect.js');
 		var id = req.body['id'];
 		var pass = req.body['pass'];
-		console.log(`ID : ${id} pass: ${pass}`);
 		dbconnect('select id, pass from users where id = "'+ id +'" and pass = "'+pass+'" limit 1;', function(err, rows){
 			if(err) {
 				console.log('Error while querying '+err);
@@ -25,19 +24,15 @@ module.exports = function(app) {
 				if(!rows[0]){
 					req.session.wrong = "Wrong Username OR Password";
 					res.redirect('/');
-					res.end();
 				} else {
 					if(rows[0].id === id) {
 						req.session.username = id;
 						req.session.login = 1;
-						console.log("At / post " + req.session.login);
 						req.session.wrong = null;
 						res.redirect('/home');
-						res.end();
 					} else {
 						req.session.wrong = "NO matching";
 						res.redirect('/');
-						res.end();
 					}
 				}
 			}
@@ -47,21 +42,17 @@ module.exports = function(app) {
 		if( req.session.username == null || req.session.login == 0){
 			req.session.wrong = "You are Logged Out.";
 			res.redirect('/');
-			res.end();
 		} else {
 			var id;
-			console.log("At home " + req.session.login);
 			if(req.session.login == 1){
 				res.render('home', {title: "Home", id: req.session.username});
 			} else {
 				req.session.wrong = "Please Login.";
 				res.redirect('/');
-				res.end();
 			}
 		}
 	});
 	app.get('/logout', function(req, res) {
-		console.log("At logout : "+ req.session.login);
 		if(req.session.login == null){
 			req.session.login = 0;
 		} else {
@@ -78,6 +69,5 @@ module.exports = function(app) {
 		// res.setHeader("location", "/");
 		// res.end();
 		res.redirect(302, '/');
-		res.end();
 	});
 }
